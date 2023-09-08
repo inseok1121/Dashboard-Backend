dfd11cf4b0b557300d863f975c16983d25ef4592
inseok1121 <inseck1121@gmail.com>
2023-09-08 17:44:26 +0900

    init

---
+++ src/test/java/com/coin/analys/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coin.analys.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
